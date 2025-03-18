e39461423a0acae68391581c8badfda0624405a9
Devraj Singh <aol.devraj@aol.com>
2025-03-18 16:27:21 +0530

    working flask api#

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import os
 import datetime
 import pytz
-from google.oauth2.credentials import Credentials
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
